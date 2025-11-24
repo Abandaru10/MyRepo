68387f93c9357464539693bbd8e4e3928c9e9af5
Abandaru10 <abandaru@deltaintech.com>
2025-11-24 17:28:33 +0530

    Added GitCOntroller

---
+++ MyfirstGitPractice/MyfirstGitPractice/Controllers/GitController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyfirstGitPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitController : ControllerBase
+    {
+        public GitController() { }
+        public GitController(string apiKey) { }
+            
+    }
+}
